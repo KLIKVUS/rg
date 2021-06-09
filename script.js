9fdf9685f647d67464b367dd9cabf23b31b7f998
admin <TimoninIV20@st.ithub.ru>
2021-06-09 19:43:25 +0300

    se

--- script.js
+++ script.js
@@ -1,42 +1,60 @@
 window.addEventListener("load", () => {
     const GameCardList = document.querySelector("#GameCardList");
+    const Search = document.forms.Search;
+    let newArr = null;
 
-    // Добавление карт
-    // const getRandomNum = (min, max) => {
-    //     return Math.floor(Math.random() * (max - min)) + min;
-    // }
+    // Рандомное значение карты
+    const getRandomNum = (min, max) => {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
     var NumberOfShownCards = 0;
     var GenerateCrads = true;
-    const ScrollCardLoad = (card) => {
-        while(NumberOfShownCards != Object.keys(PageCard).length && GenerateCrads != false) {
+    var CardLoading = false;
+    var MainInfo = null;
+
+    if (document.querySelector("#infos") == undefined) {
+        GenerateCrads = false;
+        CardLoading = false;
+        MainInfo = document.querySelector("#info");
+        // return
+    } else {
+        MainInfo = document.querySelector("#infos");
+    }
+
+    const ScrollCardLoad = (cards) => {
+        if (cards === newArr) {
+            GameCardList.innerHTML = null;
+            for (let item of newArr) {
+                GenerateCrads = false;
+                GameCardList.innerHTML += PageCard[item];
+            }
+            MainInfo.style = [
+                "display: none;"
+            ];
+        }
+
+        // while(NumberOfShownCards != Object.keys(PageCard).length && GenerateCrads != false) {
+        //     var windowPosBottom = document.documentElement.getBoundingClientRect().bottom;
+        //     if (windowPosBottom > document.documentElement.clientHeight + 100) break;
+    
+        //     GameCardList.innerHTML += PageCard[GamesNames[NumberOfShownCards]];
+        //     NumberOfShownCards++;
+        // }
+
+        while(GenerateCrads != false) {
             var windowPosBottom = document.documentElement.getBoundingClientRect().bottom;
             if (windowPosBottom > document.documentElement.clientHeight + 100) break;
-
-            // var RandomNum = getRandomNum(0, GamesNames.length);
-            // var RandGame = GamesNames[RandomNum];
-
-            // GameCardList.innerHTML += PageCard[RandGame];
-            // GamesNames.splice(RandomNum, 1);
-
-            GameCardList.innerHTML += PageCard[GamesNames[NumberOfShownCards]];
-            NumberOfShownCards++;
-        }
-
-        if (card === newArr) {
-            GenerateCrads = false;
-            GameCardList.innerHTML = PageCard[card];
+    
+            GameCardList.innerHTML += PageCard[GamesNames[getRandomNum(0, GamesNames.length)]];
+            GameCardList.innerHTML += PageCard[GamesNames[getRandomNum(0, GamesNames.length)]];
         }
     }
-    
     window.addEventListener("scroll", ScrollCardLoad);
-    ScrollCardLoad;
+    ScrollCardLoad();
 
 
     // Поиск
-    const Search = document.forms.Search
-    let newArr = null;
-
     const SearchGame = (e) => {
         e.preventDefault();
 
@@ -47,14 +65,22 @@ window.addEventListener("load", () => {
             if (exp.test(el)) return el;
         })
         
-        if (PageCard[newArr] !== undefined) {
+        if (newArr.length != "0") {
             ScrollCardLoad(newArr);
-        } else if (PageCard[newArr] === undefined) {
+        } else {
             GenerateCrads = false;
             GameCardList.innerHTML = SearchError[0];
         }
     }
 
     Search.addEventListener("submit", SearchGame);
-    Search.addEventListener("search", () => {GameCardList.innerHTML = PageCard; console.log(PageCard);})
+    Search.addEventListener("search", () => {
+        MainInfo.style = [
+            "display: block;"
+        ];
+        NumberOfShownCards = 0;
+        if (CardLoading == false) { GenerateCrads = true; }
+        GameCardList.innerHTML = null;
+        ScrollCardLoad();
+    })
 })
--- scripts/cards.js
+++ scripts/cards.js
@@ -1,14 +1,97 @@
 var PageCard = {
-    GameName1: `<div class="card text-white bg-dark border-secondary mb-3"><img src="images/test.png" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></div></div>`,
-    GameName2: `<div class="card text-white bg-dark border-secondary mb-3"><img src="" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></div></div>`,
-    GameName3: `<div class="card text-white bg-dark border-secondary mb-3"><img src="" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></div></div>`,
-    GameName4: `<div class="card text-white bg-dark border-secondary mb-3"><img src="" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></div></div>`,
-    GameName5: `<div class="card text-white bg-dark border-secondary mb-3"><img src="" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></div></div>`
+    "Don't Go Into The Basement": `
+    <div class="col">
+    <div class="card text-white bg-dark border-secondary">
+        <img src="https://img.itch.zone/aW1nLzU5Mjg3MTQucG5n/315x250%23c/N%2B1xlv.png" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Don't Go Into The Basement</h5>
+        <p class="card-text">This is our first game - Hope you enjoy</p>
+        <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </p>
+        </div>
+        <a href="" class="text"><div class="card-footer">
+            <small class="text-muted">Читать больше</small>
+        </div></a>
+    </div>
+    </div>`,
+
+    "No Players Online": `
+    <div class="col">
+    <div class="card text-white bg-dark border-secondary">
+        <img src="https://img.itch.zone/aW1nLzI2NTgzMDEuZ2lm/original/ND9VyR.gif" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">No Players Online</h5>
+        <p class="card-text">You find a dusty vhs tape, on the side is written: "capture the flag project, footage 03/20/86" you decide to put it into the player.</p>
+        <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </p>
+        </div>
+        <a href="" class="text"><div class="card-footer">
+            <small class="text-muted">Читать больше</small>
+        </div></a>
+    </div>
+    </div>`,
+
+    "Super Bat Puncher Demo (NES)": `
+    <div class="col">
+    <div class="card text-white bg-dark border-secondary">
+        <img src="https://img.itch.zone/aW1nLzYwNDIxNzYucG5n/315x250%23c/Xl3%2BS%2B.png" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Super Bat Puncher Demo (NES)</h5>
+        <p class="card-text">In the distant future, earth is haunted by extraterrestrial bats. They seem to appear out  of nowhere, but always come in large numbers, are highly susceptible to shock and tend to explode quickly...</p>
+        <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </p>
+        </div>
+        <a href="" class="text"><div class="card-footer">
+            <small class="text-muted">Читать больше</small>
+        </div></a>
+    </div>
+    </div>`,
+
+    "The House In The Woods": `
+    <div class="col">
+    <div class="card text-white bg-dark border-secondary">
+        <img src="https://img.itch.zone/aW1nLzMzMDk0MjUucG5n/315x250%23c/zy9U5C.png" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">The House In The Woods</h5>
+        <p class="card-text">On a camping trip into the forest your friend has vanished in the night.  As you step away from your camp to investigate you realise you are lost as well...</p>
+        <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </p>
+        </div>
+        <a href="" class="text"><div class="card-footer">
+            <small class="text-muted">Читать больше</small>
+        </div></a>
+    </div>
+    </div>`,
+
+    "Survius": `
+    <div class="col">
+    <div class="card text-white bg-dark border-secondary">
+        <img src="https://img.itch.zone/aW1nLzE2MDk5MDYucG5n/315x250%23c/MhOzJZ.png" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Survius</h5>
+        <div class="card-text">
+            <p>Survius is an Open World Multiplayer Zombie Survival Game</p>
+            
+            <p>The game is set on an island named Survius. The island and it's settlements seem to have been deserted. You scavenge the landscape looking for water, food and shelter.</p>
+        </div>
+        <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </p>
+        </div>
+        <a href="" class="text"><div class="card-footer">
+            <small class="text-muted">Читать больше</small>
+        </div></a>
+    </div>
+    </div>`
 };
 
-var GamesNames = ["GameName1", "GameName2", "GameName3", "GameName4", "GameName5"];
+var GamesNames = ["Don't Go Into The Basement", "No Players Online", "Super Bat Puncher Demo (NES)", "The House In The Woods", "Survius"];
 
 var SearchError = [
-    `<div class="card text-white bg-dark border-secondary mb-3"><img src="" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></div></div>`,
+    `ОШИБКА`,
     `<div class="card text-white bg-dark border-secondary mb-3"><img src="" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p><p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></div></div>`
 ];
